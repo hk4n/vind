2dc3fb4b514629e072f96d8d19651ae4ab20c5af
hk4n <hakan@focotech.se>
2017-04-29 22:01:15 +0200

    Now both absolute and relative file path's works

--- vim-run.sh
+++ vim-run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-IMAGE=$(docker images | grep -q "$USER-vim"; echo $?)
+# Check if absolute or relative path of the first argument.
+# If absolute the "dirname" of the file location will be
+# used for the mount point to the docker container
+if [[ "$1" = /* ]]; then
+    MOUNT=$(dirname "$1")
+else
+    MOUNT=$PWD
+fi
 
-[[ $IMAGE == 1 ]] && docker build -t "$USER-vim" -f Dockerfile.vim .
-
-docker run -it --rm -v "$PWD:$PWD" --workdir "$PWD" "$USER-vim" "$USER" "$UID" "$(id -g "$USER")" "$@"
+docker run -it --rm -v "$MOUNT:$MOUNT" --workdir "$MOUNT" "$USER-vim" "$USER" "$UID" "$(id -g "$USER")" "$@"
