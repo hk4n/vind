aa34392076f58c4a169b3475d31517db0ef19c85
hk4n <hakan@focotech.se>
2017-05-01 15:25:36 +0200

    added support for .. relative path's in vim-run.sh

--- vim-run.sh
+++ vim-run.sh
@@ -5,8 +5,13 @@
 # used for the mount point to the docker container
 if [[ "$1" = /* ]]; then
     MOUNT=$(dirname "$1")
+    WORKDIR=$MOUNT
+elif [[ "$1" = ..* ]]; then
+    MOUNT="$(readlink -f "$(dirname "$1")")"
+    WORKDIR=$PWD
 else
     MOUNT=$PWD
+    WORKDIR=$MOUNT
 fi
 
-docker run -it --rm -v "$MOUNT:$MOUNT" --workdir "$MOUNT" "$USER-vim" "$USER" "$UID" "$(id -g "$USER")" "$@"
+docker run -it --rm -v "$MOUNT:$MOUNT" -v "$WORKDIR:$WORKDIR" --workdir "$WORKDIR" "$USER-vim" "$USER" "$UID" "$(id -g "$USER")" "$@"
